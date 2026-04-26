94656bdc11a5813c05f5399b839a5669a527dbf9
Diamond the Novabeast <diamondprotoreal@gmail.com>
2026-04-26 13:25:09 -0500

    .

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-100*Math.pow(rank-1, 0.648) + 1000) *
+    let score = (-100*Math.pow(rank-1, 0.6475) + 1000) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
