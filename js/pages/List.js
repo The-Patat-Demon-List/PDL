97fa364574bd7e602d6ffc936f6a955b2c4b0123
Diamond the Novabeast <diamondprotoreal@gmail.com>
2026-04-27 14:48:48 -0500

    .

--- js/pages/List.js
+++ js/pages/List.js
@@ -107,13 +107,10 @@ export default {
                     </p>
                     <p>
                         If you are submitting for an extreme or insane demon clicks & audio is required
-
                     <p>
                         If you are submitting for a hard demon a video is required
-
                     <p>
                         If you are submitting for an easy or medium demon a screenshot of the endscreen is required
-
                     </p>
                     <p>
                          For an extreme or insane demon the recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
