b09522009a655ca36f9c1d99e4cba96f45a5260d
Diamond the Novabeast <diamondprotoreal@gmail.com>
2026-04-28 15:22:09 -0500

    .

--- js/content.js
+++ js/content.js
@@ -103,6 +103,7 @@ export async function fetchLeaderboard() {
                 percent: record.percent,
                 score: score(rank + 1, record.percent, level.percentToQualify),
                 link: record.link,
+                user: record.user,
             });
         });
     });
