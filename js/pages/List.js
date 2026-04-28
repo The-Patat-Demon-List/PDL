dd2d3eb787e2551b69911d97da168acc223c5449
Diamond the Novabeast <diamondprotoreal@gmail.com>
2026-04-28 10:47:34 -0500

    .

--- js/pages/List.js
+++ js/pages/List.js
@@ -39,7 +39,7 @@ export default {
             <div class="level-container">
                 <div class="level" v-if="level">
                     <h1 style="display:flex; align-items:center; gap:8px;">
-                        <div>
+                        <div style="display:flex; align-items:center; gap:8px; flex:1;">
                             <img :src="getDemonRating()" class="difficultyface" v-if="level.difficulty"/>
                             <span style="font-weight:600; font-size:1.1em;">{{ level.name }}</span>
                         </div>
